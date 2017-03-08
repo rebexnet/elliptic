04dd650f30d1692d08db38b0b30211d0096d484a
Lukas Paluzga <lukas.paluzga@rebex.cz>
2017-03-08 17:07:35 +0100

    Xamarin Android for Rebex.Castle support

---
+++ Src/Rebex.Castle.Android/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Android.App;
+
+[assembly: AssemblyTitle("Rebex.Castle")]
+[assembly: AssemblyProduct("Rebex.Castle")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
