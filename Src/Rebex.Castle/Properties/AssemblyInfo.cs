ad5fe6772be6ff42577ede9161b9a25f85b0a4e9
Lukas Pokorny <lukas@nuane.com>
2019-11-27 13:07:02 +0100

    Upgraded Rebex.Castle.CF to .NET CF 3.5

--- Src/Rebex.Castle/Properties/AssemblyInfo.cs
+++ Src/Rebex.Castle/Properties/AssemblyInfo.cs
@@ -1,9 +1,6 @@
-﻿#if NETCF_1_0
-using System;
-[assembly: CLSCompliant(true)]
-#endif
+﻿using System;
 
-#if ANDROID || IOS || PORTABLE
+#if NETCF_1_0 || NETCF_2_0
 using System.Reflection;
 using System.Runtime.InteropServices;
 
@@ -11,12 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Rebex.Castle")]
 [assembly: AssemblyCopyright("Copyright © Rebex.NET 2017")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: ComVisible(false)]
 #endif
 
-#if IOS
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3ffd0cf2-a4b5-45ea-b381-46fa5b09a77b")]
-#endif
+[assembly: CLSCompliant(true)]
